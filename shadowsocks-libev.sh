b6c4e667a09e239536e9d2eb5a73a01be0202df7
leesiyang <mrlbz@hotmail.com>
2016-06-01 15:42:33 +0800

    Update shadowsocks-libev.sh

--- shadowsocks-libev.sh
+++ shadowsocks-libev.sh
@@ -65,7 +65,7 @@ function pre_install(){
     #Set shadowsocks-libev config password
     echo "Please input password for shadowsocks-libev:"
     read -p "(Default password: teddysun.com):" shadowsockspwd
-    [ -z "$shadowsockspwd" ] && shadowsockspwd="teddysun.com"
+    [ -z "$shadowsockspwd" ] && shadowsockspwd="abc.com"
     echo ""
     echo "---------------------------"
     echo "password = $shadowsockspwd"
